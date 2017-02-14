fb7f0abce3a21b4ab3024441a4ff50f316b51aa2
garyy <garyy@DESKTOP-UPRM89O>
2017-02-13 20:04:42 -0500

    Removed test classes

--- src/edu/buffalo/cse116/TestClass.java
+++
@@ -1,7 +0,0 @@
-package edu.buffalo.cse116;
-
-public class TestClass {
-public static void main(String[] args){
-	System.out.println("hi");
-}
-}
--- src/edu/buffalo/cse116/TestClassAgain.java
+++
@@ -1,7 +0,0 @@
-package edu.buffalo.cse116;
-
-public class TestClassAgain {
-	public static void main(String[] args){
-		System.out.println("hi");
-	}
-}
