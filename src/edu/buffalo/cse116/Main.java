5680dc1d865eb12fa0ae42580da2cbfddca7865b
garyy <garyy@DESKTOP-UPRM89O>
2017-02-16 19:57:41 -0500

    Worked on writing the main method. Didn't create update for xCalc/yCalc, or set current point escape time.

--- src/edu/buffalo/cse116/Main.java
+++ src/edu/buffalo/cse116/Main.java
@@ -1,10 +1,26 @@
 package edu.buffalo.cse116;
 
-public class Main {
+import java.awt.Point;
 
+public class Main {
+/*
+	Worked on writing the main method. Didn't create update for xCalc/yCalc, or set current point escape time.
+	*/ 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
-
+	 int [][] grid = new int[512][512];
   }
-
+  public Point escapeTimeAlgorithm(Point point){
+	  float xCalc = point.x;
+	  float yCalc = point.y;
+	  double dist = Math.pow(Math.pow(xCalc, 2) + Math.pow(yCalc, 2),-2);
+	  int passes = 0;
+	  while(dist <= 4 && passes < 255){
+		  passes = passes + 1;
+		  dist = Math.pow(Math.pow(xCalc, 2) + Math.pow(yCalc, 2),-2);
+	  }
+	  
+	  Point retPoint = new Point(0,0);
+	  return retPoint;
+  }
 }
