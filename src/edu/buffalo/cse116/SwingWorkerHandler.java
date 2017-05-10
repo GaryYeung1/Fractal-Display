219791de9b51796b95887281b8f3697d67c2f4da
flore <flore@DESKTOP-N5BANO1>
2017-05-10 15:24:02 -0400

    added more to swingworkerhandler

--- src/edu/buffalo/cse116/SwingWorkerHandler.java
+++ src/edu/buffalo/cse116/SwingWorkerHandler.java
@@ -10,13 +10,17 @@ public class SwingWorkerHandler extends SwingWorker<WorkerResult,Void> {
 	private int _workers; 
 	private String fractal = "";
 	private int rowToStart = 0;
-	private int rowsToCreate = 0; 
+	private int rowsToCreate = 0;
+	private int[][] workGrid;
 	
-	public SwingWorkerHandler(int i, int j, int[][] grid, int escapeTime){
+	public SwingWorkerHandler(int i, int j, int[][] grid, int escapeTime,int workers){
 		_grid= grid;
 		_escapeTime = escapeTime;
 		rowToStart = i;
 		rowsToCreate = j;
+		_workers= workers;
+		int rows = 2048/_workers;
+		workGrid = new int[rows][2048];
 	}
 	//calculates in parallelism
 	//Calculating results should be split as equally as possible along the rows of the 
@@ -25,7 +29,14 @@ public class SwingWorkerHandler extends SwingWorker<WorkerResult,Void> {
 	@Override
 	protected WorkerResult doInBackground() throws Exception {
 		// TODO Auto-generated method stub
+		int rows = 2048/_workers;
+		int[][] workGrid = new int[rows][2048];
+		for(int i = 0; i<rows;i++){
+			for(int j = 0; j<2048; i++){
+			workGrid[i][j]= _grid[rowToStart+i][j];}
+		}
 
+		WorkerResult workerRes = new WorkerResult(rowToStart, workGrid);
 		
 		return null;
 	}
